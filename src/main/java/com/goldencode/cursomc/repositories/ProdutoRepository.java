faa61c0ad2c40562ba50dcbe91c63cc346984071
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2020-09-22 20:30:39 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/goldencode/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.goldencode.cursomc.repositories;
+
+import com.goldencode.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
