16d54aac89df79f2ab2d95a64a9043f80ad335a2
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2020-09-22 22:07:31 -0300

    Estado e Cidade

--- src/main/java/com/goldencode/cursomc/CursomcApplication.java
+++ src/main/java/com/goldencode/cursomc/CursomcApplication.java
@@ -1,8 +1,12 @@
 package com.goldencode.cursomc;
 
 import com.goldencode.cursomc.domain.Categoria;
+import com.goldencode.cursomc.domain.Cidade;
+import com.goldencode.cursomc.domain.Estado;
 import com.goldencode.cursomc.domain.Produto;
 import com.goldencode.cursomc.repositories.CategoriaRepository;
+import com.goldencode.cursomc.repositories.CidadeRepository;
+import com.goldencode.cursomc.repositories.EstadoRepository;
 import com.goldencode.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,6 +23,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -45,6 +55,19 @@ public class CursomcApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 	}
 }
