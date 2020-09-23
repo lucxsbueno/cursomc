dda5c2d6318344443d04ca17136b15da02099cbb
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2020-09-23 11:13:37 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/goldencode/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,10 @@
+package com.goldencode.cursomc.repositories;
+
+import com.goldencode.cursomc.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
