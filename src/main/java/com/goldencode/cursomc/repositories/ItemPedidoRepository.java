e4682115d8e41c0ad85af3ba2e05aa7bebbfe397
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2020-09-23 21:31:45 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/goldencode/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.goldencode.cursomc.repositories;
+
+import com.goldencode.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
