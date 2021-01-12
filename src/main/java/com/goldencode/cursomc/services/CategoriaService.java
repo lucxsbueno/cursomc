57158d5bbb64ceec5e7287bed5f7fab4ee57d021
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2021-01-12 18:33:21 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/goldencode/cursomc/services/CategoriaService.java
+++ src/main/java/com/goldencode/cursomc/services/CategoriaService.java
@@ -9,6 +9,9 @@ import net.bytebuddy.implementation.bytecode.Throw;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -49,6 +52,11 @@ public class CategoriaService {
 
     public List<Categoria> findAll() {
     	return repo.findAll();
+    }
+    
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+    	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+    	return repo.findAll(pageRequest);
     }
     
 }
