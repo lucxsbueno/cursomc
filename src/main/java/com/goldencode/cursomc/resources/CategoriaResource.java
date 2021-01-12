302c84d54f3d27066c9a721c47d0f8452af722d2
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2021-01-12 18:00:18 -0300

    Listando todas as Categorias com DTO (Data Transfer Object)

--- src/main/java/com/goldencode/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/goldencode/cursomc/resources/CategoriaResource.java
@@ -1,9 +1,12 @@
 package com.goldencode.cursomc.resources;
 
 import com.goldencode.cursomc.domain.Categoria;
+import com.goldencode.cursomc.dto.CategoriaDTO;
 import com.goldencode.cursomc.services.CategoriaService;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -44,10 +47,15 @@ public class CategoriaResource {
     
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id){
-
         service.delete(id);
         return ResponseEntity.noContent().build();
-
     }
 
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+    
 }
--- src/main/java/com/goldencode/cursomc/services/CategoriaService.java
+++ src/main/java/com/goldencode/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -44,7 +45,10 @@ public class CategoriaService {
 			throw new 
 				DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
 		}
-   
     }
 
+    public List<Categoria> findAll() {
+    	return repo.findAll();
+    }
+    
 }
