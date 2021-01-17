b315a6a2172dcddcfa7fe8aee6d753d7140c5f68
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2021-01-17 05:58:09 -0300

    Personalizando o retorno da validação

--- src/main/java/com/goldencode/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/goldencode/cursomc/resources/exceptions/StandardError.java
@@ -1,7 +1,8 @@
 package com.goldencode.cursomc.resources.exceptions;
 
-public class StandardError {
+import java.io.Serializable;
 
+public class StandardError  implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer status;
